a593f337aa0f684d22b89d1cffe60d3d2ba20baa
Jonathan Perry <jpper@umich.edu>
2020-11-30 14:40:15 +0900

    added button above card; created a statefulwidget and moved build into a state object referenced by the new class widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,42 @@ main() {
   runApp(ProductsApp());
 }
 
-class ProductsApp extends StatelessWidget {
+class ProductsApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // Return the state
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<ProductsApp> {
   @override
   Widget build(context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Product List"),
-        ),
-        body: Card(
-          child: Column(
-            children: [Image.asset("assets/beach.jpg"), Text("Food Paradise")],
+          appBar: AppBar(
+            title: Text("Product List"),
           ),
-        ),
-      ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text("Add Product"),
+                ),
+              ),
+              Card(
+                child: Column(
+                  children: [
+                    Image.asset("assets/beach.jpg"),
+                    Text("Food Paradise")
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
